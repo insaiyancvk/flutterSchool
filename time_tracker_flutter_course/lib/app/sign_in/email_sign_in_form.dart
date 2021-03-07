8a591c18e4c6e9af62fde3d09871c3f29e55b7d7
insaiyancvk <cvamshik1@gmail.com>
2021-03-07 22:02:41 +0530

    added variables for storing mail id and password

--- time_tracker_flutter_course/lib/app/sign_in/email_sign_in_form.dart
+++ time_tracker_flutter_course/lib/app/sign_in/email_sign_in_form.dart
@@ -3,19 +3,29 @@ import 'package:flutter/material.dart';
 import 'common_widgets/form_submit_button.dart';
 
 class EmailSignInForm extends StatelessWidget {
+  final TextEditingController _emailcontroller = TextEditingController();
+  final TextEditingController _passwordcontroller = TextEditingController();
+  String email;
+
   void _submit() {
     // TODO: print email and password
+    print(
+        "email: ${_emailcontroller.text} \n password: ${_passwordcontroller.text}");
   }
 
   List<Widget> _buildChildren() {
     return [
       TextField(
-          decoration: InputDecoration(
-        labelText: 'Email',
-        hintText: 'abc@xyz.com',
-      )),
+        controller: _emailcontroller,
+        decoration: InputDecoration(
+          labelText: 'Email',
+          hintText: 'abc@xyz.com',
+        ),
+        onChanged: (value) => email = value,
+      ),
       SizedBox(height: 8.0),
       TextField(
+          controller: _passwordcontroller,
           decoration: InputDecoration(
             labelText: 'Password',
           ),
@@ -23,7 +33,7 @@ class EmailSignInForm extends StatelessWidget {
       SizedBox(height: 8.0),
       FormSubmitButton(
         text: ('Sign In'),
-        onPressed: () {},
+        onPressed: _submit,
       ),
       SizedBox(height: 8.0),
       FlatButton(
